68bfad136ac10fb8133cec23610daf947a291e57
Eric Jones <eric@recovery-compass.org>
2025-06-23 01:45:30 -0700

    feat: Create environmental-response-design project and fix vulnerabilities

---
+++ environmental-response-design/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen bg-gray-900 flex justify-center items-center text-white">
+      <h1 className="text-4xl">Environmental Response Design</h1>
+    </div>
+  )
+}
+
+export default App
